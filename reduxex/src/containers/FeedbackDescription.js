7bb5da8e35f293e47386197831cc49ffff706f74
Mallick <683437@cognizant.com>
2018-09-17 17:45:33 +0530

    updated

--- reduxex/src/containers/FeedbackDescription.js
+++ reduxex/src/containers/FeedbackDescription.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import descriptionAction from '../actions/updateDescriptionAction'
+import updateDescriptionAction from '../actions/updateDescriptionAction'
 import { bindActionCreators } from 'redux';
 
 class FeedBackDescription extends React.Component {
@@ -10,6 +10,7 @@ class FeedBackDescription extends React.Component {
             description: {}
         }
         this.updateDescription = this.updateDescription.bind(this)
+        this.submitForm = this.submitForm.bind(this)
     }
     updateDescription(e) {
         const temp = Object.assign({}, this.props.selectedFBprops, {
@@ -18,9 +19,10 @@ class FeedBackDescription extends React.Component {
         this.setState({
             description: temp
         })
-        console.log('updated description', this.state.description)
+        console.log( "Input updated")
     }
     submitForm(e) {
+        console.log("Inside submit")
         this.props.updatedDescription(this.state.description)
     }
     render() {
@@ -38,10 +40,9 @@ class FeedBackDescription extends React.Component {
                 <li>{this.props.selectedFBprops.description}</li>
             </ul>
             <div>
-                <form onSubmit={() => this.submitForm}>
+                <form onSubmit={this.submitForm}>
                     Update Description:
                     <input type="text"
-                        value={this.state.description.title}
                         onChange={this.updateDescription} />
                     <br />
                     <input type="submit" />
@@ -56,6 +57,6 @@ function connentStateToComponentAsProps(state) {
     })
 }
 function bindActionCreatorToComponentAsProps(dispatch) {
-    return bindActionCreators({ updatedDescription: descriptionAction }, dispatch)
+    return bindActionCreators({ updatedDescription: updateDescriptionAction }, dispatch)
 }
 export default connect(connentStateToComponentAsProps, bindActionCreatorToComponentAsProps)(FeedBackDescription);
--- reduxex/src/reducers/update-description-reducer.js
+++ reduxex/src/reducers/update-description-reducer.js
@@ -1,7 +1,8 @@
 const descriptionUpdated = function (state = null, action) {
     switch (action.type) {
         case "DESCRIPTION_UPDATED":
-        console.log('desc updated')
+            console.log('desc updated')
+            console.log(action.payload)
             return action.payload
             break;
         default:
